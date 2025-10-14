24c4a355ca628fa6cab8ef5255d4bd5b3b6c2f32
FarmerChazmann <measuresilver2@gmail.com>
2025-10-14 21:35:27 +0800

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -239,7 +239,7 @@ async function fetchFieldRows(_userId) {
       field_group,
       area_ha,
       perimeter_m,
-      field_boundary:field_boundary::text,
+      field_boundary:ST_AsGeoJSON(field_boundary)::json,
       properties,
       updated_at,
       farms (
